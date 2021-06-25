f936b7507a7194443f0e1d3cb6c40fc404f794ce
jonghun99 <dlawhdgns99@naver.com>
2021-06-25 14:58:05 +0900

    2021. 06. 25. (금) 14:58:05 KST

---
+++ shell_var.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SMART1=100
+echo '$SMART1='$SMART1
+echo '$SMART1='${SMART1:-200}
+echo '$SMART2='${SMART2:-200}
+echo '$SMART3='${SMART3:=300}
+
+SMART4=
+echo '$SMART4='$SMART4
+echo '$SMART4='${SMART4:=400}
+
+SMART5=
+echo '$SMART5='${SMART5:+500}
+SMART5=100
+echo '$SMART5='${SMART5:+500}
+
+SMART6=600
+echo '$SMART6='${SMART6:?600}
+
+SMART6=
+echo '$SMART6='${SMART6:?600}
+
+echo '$SMART6='${SMART6:?"번의 값이 없습니다."}
+echo ================================
+echo ================================
+echo ================================
+echo ================================
